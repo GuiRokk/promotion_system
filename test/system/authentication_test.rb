9cc08ebb45a65a89459e115b3e7299870fc85eb3
Gui Roque <gui.roque@outlook.com.br>
2021-03-25 14:48:28 -0300

    WIP Testes de autenticação

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -12,7 +12,7 @@ class AuthenticationTest < ApplicationSystemTestCase
     within 'form' do
       click_on 'Cadastrar'
     end
-    
+
     assert_text "Boas Vindas! Cadastrou e entrou com sucesso"
     assert_text 'jane.doe@iugu.com.br'
     assert_link 'Sair'
@@ -42,8 +42,84 @@ class AuthenticationTest < ApplicationSystemTestCase
   end
 
 
-  #TODO: TESTE LOGOUT
-  #TODO: TESTE FALHA AO REGISTRAR
+  test 'user sign out' do
+    user = User.create!(email:'jane.doe@iugu.com.br', password:'password')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Log in'
+    click_on 'Sair'
+
+    assert_current_path root_path
+    assert_text 'Saiu com sucesso'
+    assert_no_link "Promoção"
+    assert_no_link "Produtos"
+    assert_no_link "Sair"
+    assert_link 'Cadastrar'
+    assert_link 'Entrar'
+  end
+
+  test 'user fails to register: blanks' do
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with:''
+    fill_in 'Senha', with: ''
+    within 'form' do
+      click_on 'Cadastrar'
+    end
+
+    assert_text 'Não foi possível salvar'
+    assert_text 'não pode ficar em branco', count:2
+  end
+
+  test 'user fails to register: email must be unique' do
+    user = User.create!(email:'jane.doe@iugu.com.br', password:'password')
+    
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirmação de senha', with: 'password'
+    within 'form' do
+      click_on 'Cadastrar'
+    end
+
+    assert_text 'Não foi possível salvar'
+    assert_text 'Email já está em uso'
+  end
+
+  test 'user fails to register: password must be 6 characters minimun' do
+
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: 'john.doe@iugu.com.br'
+    fill_in 'Senha', with: '12345'
+    fill_in 'Confirmação de senha', with: '12345'
+    within 'form' do
+      click_on 'Cadastrar'
+    end
+
+    assert_text 'Não foi possível salvar'
+    assert_text 'Password é muito curto (mínimo: 6 caracteres)'
+  end
+
+  test "user fails to register: password confirmation doen't match password" do
+
+    visit root_path
+    click_on 'Cadastrar'
+    fill_in 'Email', with: 'john.doe@iugu.com.br'
+    fill_in 'Senha', with: '12345'
+    fill_in 'Confirmação de senha', with: '1234'
+    within 'form' do
+      click_on 'Cadastrar'
+    end
+
+    assert_text 'Não foi possível salvar'
+    assert_text 'Password confirmation não é igual a Password'
+  end
+
   #TODO: TESTE FALHA AO LOGAR
   #TODO: TESTE EDITAR USUARIO
   #TODO: I18n USER
