d6d4e2fba78a8818e2fba08b3e4904d317e419c8
Gui Roque <gui.roque@outlook.com.br>
2021-04-03 16:50:51 -0300

    WIP API

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {registrations:  'registrations'}
   resources :users, only: [:show]
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :coupons, only: [:show], param: :code
+    end
+  end
+
 end
