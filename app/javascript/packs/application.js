c605d8a2ce5ff1506cc6e3ef71db225abf144988
Gui Roque <gui.roque@outlook.com.br>
2021-04-08 14:23:17 -0300

    Add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,17 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import * as bootstrap from 'bootstap'
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
+
+
+
 
 Rails.start()
 Turbolinks.start()
